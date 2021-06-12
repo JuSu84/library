881b7c2cd9afa272403d392a417dcf2ae1aeeed2
Paweł Bajda <pawel.bajda@gmail.com>
2021-06-12 14:22:20 +0200

    Add class Library

---
+++ src/main/java/Library.java
@@ -0,0 +1,22 @@
+public class Library {
+    public static void main(String[] args) {
+        final String appName = "Bliblioteka v0.1";
+
+        String title = "Wiedźmin. Ostatnie życzenie";
+        String author = "Andrzej Sapkowski";
+        String namePublishingHouse = "NOWA sp. z p.o.";
+        int relaseDate = 2011;
+        int pages = 286;
+        String isbnNumber = "978-83-7578-028-4";
+
+        System.out.println(appName);
+        System.out.println("Książki dostępne w bibliotece: ");
+        System.out.println(title);
+        System.out.println(author);
+        System.out.println(namePublishingHouse);
+        System.out.println(relaseDate);
+        System.out.println(pages);
+        System.out.println(isbnNumber);
+
+    }
+}
