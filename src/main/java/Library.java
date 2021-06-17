a2f226ad6534fe0c2fc952aaf9e70b333ace8340
Paweł Bajda <pawel.bajda@gmail.com>
2021-06-17 09:10:22 +0200

    Add package app and model

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,19 +1,32 @@
-public class Library {
+
+/*
+ * Copyright (c) 2021. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+// author Paweł Bajda
+
+import model.Book;
+
+class Library {
     public static void main(String[] args) {
-        final String appName = "Bliblioteka v0.1";
+        final String appName = "Bliblioteka v0.3";
 
-        Book book1 = new Book("Wiedźmin. Ostatnie życzenie", "Wiedźmin. Ostatnie życzenie",
-                "NOWA sp. z p.o.", 2011, 286, "978-83-7578-028-4" );
-
+//        Add new book to Library
+        Book book1 = new Book("Wiedźmin. Ostatnie życzenie", "Andrzej Sapkowski",
+                "NOWA sp. z p.o.", 2011, 286, "978-83-7578-028-4");
+        Book book2 = new Book("Finansowy Ninja", "Michał Szafrański",
+                "Kaveo Publishing", 2019, 539, "978-83-65308-05-4");
+        Book book3 = new Book("Narrenturm", "Andrzej Sapkowski",
+                "NOWA sp. z p.o.", 2002, 592);
 
         System.out.println(appName);
-        System.out.println("Książki dostępne w bibliotece: ");
-        System.out.println(book1.title);
-        System.out.println(book1.author);
-        System.out.println(book1.namePublishingHouse);
-        System.out.println(book1.relaseDate);
-        System.out.println(book1.pages);
-        System.out.println(book1.isbnNumber);
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(book3);
 
 
     }
